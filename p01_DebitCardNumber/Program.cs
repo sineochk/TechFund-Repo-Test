fe568ab07049b31add26c3f240c060c8466c3b28
sineochk <Andzhelika93@gmail.com>
2017-06-05 09:13:13 +0300

    added 1st exercise - Debit card number

---
+++ p01_DebitCardNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace p01_DebitCardNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+                
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("{0:D4} ", num[i]);
+                //Console.Write("{0:0000} ", num[i]);
+            }
+            
+        }
+    }
+}
